47bd03cac457b0477d950165df106fc8b1c3396c
Andrea <andrea.apicella221@gmail.com>
2022-05-14 20:42:12 +0200

    improved custom keras time series generator

--- wp8/pre_processing/generators.py
+++ wp8/pre_processing/generators.py
@@ -5,7 +5,7 @@ from tensorflow.keras.utils import Sequence
 
 
 class TimeSeriesGenerator(Sequence):
-    def __init__(self, X, y: list, num_features: int, cams: list, seq_len: int, stride: int, batch_size: int):
+    def __init__(self, X, y: list, num_features: int, cams: list, seq_len: int, stride: int, batch_size: int, evaluate=False):
         self.X = X
         self.y = y
         self.num_features = num_features
@@ -16,15 +16,18 @@ class TimeSeriesGenerator(Sequence):
         self.n = len(self.X)
         self.count = 1
         self.len = len(self.X)
+        self.evaluate = evaluate
+        self.n_windows = (self.batch_size - self.seq_len) // self.stride + 1
+        self.n_series_labels = self.n_windows * \
+            (len(self.y) // self.batch_size)
+
+        self.series_labels = []
+
+        self.ys_count = 0
+        self.get_item_calls = 0
 
     def __len__(self):
-        return self.X.shape[0] // self.batch_size
-
-    # def __pad(self, series, value):
-    #     # pad time series if it is too short
-    #     if len(series.shape[1] < self.seq_len):
-    #         # np.pad(series)
-    #         return series
+        return self.X.shape[0] // (self.batch_size)
 
     def __get_data(self, batch):
         # generate one time series of seq_len padded if its too short
@@ -32,37 +35,45 @@ class TimeSeriesGenerator(Sequence):
         X = batch["features"]
         y = batch["labels"]
         cams = batch["cams"]
-        time_series = []
-        y_s = []
-        s = 0
-        while s + self.seq_len <= (X.shape[0]):
+
+        time_series = [np.empty(self.num_features)] * self.n_windows
+        y_s = np.empty(shape=(self.n_windows,), dtype=np.uint8)
+        # s = 0
+        for w in range(self.n_windows):
+            s = w * self.stride
+            # while s + self.seq_len <= (X.shape[0]):
             features_seq = X[s:s + self.seq_len]
             labels_seq = y[s:s + self.seq_len]
+            # print(f"len(labels_seq): {len(labels_seq)}")
             cams_seq = cams[s:s + self.seq_len]
             curr_cam = mode(cams_seq)
             for i, _ in enumerate(cams_seq):
                 if cams_seq[i] != curr_cam:
                     features_seq[i] = np.zeros(self.num_features)  # padding
                     labels_seq[i] = -1  # padding
-            time_series.append(features_seq)
+            time_series[w] = features_seq
+
+            # convert time-step labels in one label per time-series
             labels_seq = filter(lambda l: l != -1, labels_seq)
-            label = mode(labels_seq)  # label with most occurrence
-            y_s.append(label)
-            s += self.stride
-            # print(
-            #     f"np.array(time_series.shape) {np.array(time_series).shape}")
-            # print(f"np.array(y_s).shape {np.array(y_s).shape}")
-        return np.asarray(time_series), np.asarray(y_s)
+            label = int(mode(labels_seq))  # label with most occurrence
+            # print(f"label after mode: {label}")
+            y_s[w] = label
+            # s += self.stride
+        # print(f"len(y_s): {len(y_s)}")
+        if not self.evaluate:
+            self.series_labels.extend(y_s)
+            self.ys_count += len(y_s)
+        return np.asarray(time_series), y_s
 
     def __getitem__(self, index):
-        # print(index)
+        self.get_item_calls += 1
         a = index * self.batch_size
-        b = (index + 1) * self.batch_size * self.seq_len
+        b = (index + 1) * self.batch_size
+        # batch_id += 1
 
-        batch = {"features": self.X[a:b + 1],
-                 "labels": self.y[a:b + 1], "cams": self.cams[a:b + 1]}
+        batch = {"features": self.X[a:b],
+                 "labels": self.y[a:b], "cams": self.cams[a:b]}
         X, y = self.__get_data(batch)
-        # print(f"X shape: {X.shape}, y shape:{y.shape}")
         return X, y
 
     def __on_epoch_end(self):

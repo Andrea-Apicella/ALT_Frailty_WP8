b550e30d15d8b11d51f0bae60e94a6f2c49f2dad
Andrea <andrea.apicella221@gmail.com>
2022-05-17 11:16:21 +0200

    fixed imports, added comments

--- wp8/pre_processing/template_match.py
+++ wp8/pre_processing/template_match.py
@@ -11,6 +11,8 @@ from pre_processing.utils import listdir_nohidden_sorted
 
 
 class TemplateMatch:
+    """Extraction of timestamps and datalogger time vectors from the NGIMU software video capture using Template Matching algorithm"""
+
     def __init__(
         self,
         video_path,
@@ -22,12 +24,8 @@ class TemplateMatch:
     ):
         self.templates_path = "./data/templates"
 
-        self.templates = [
-            cv2.imread(template)
-            for template in listdir_nohidden_sorted(self.templates_path)
-        ]
-        print(
-            f"Found {len(self.templates)} templates in {self.templates_path}.")
+        self.templates = [cv2.imread(template) for template in listdir_nohidden_sorted(self.templates_path)]
+        print(f"Found {len(self.templates)} templates in {self.templates_path}.")
 
         self.TARGET_TEMPLATE_SIZE = target_template_size
         self.TARGET_TIMESTAMP_SIZE = target_timestamp_size
@@ -39,9 +37,7 @@ class TemplateMatch:
         if element_type in ["timestamp", "datalogger"]:
             self.element_type = element_type
         else:
-            print(
-                'Specified type of element to extract is invalid. Choose "timestamp" or "datalogger".'
-            )
+            print('Specified type of element to extract is invalid. Choose "timestamp" or "datalogger".')
             return
 
         def total_frames(video_path):
@@ -60,9 +56,7 @@ class TemplateMatch:
             template = cv2.bitwise_not(template)
             return template.astype(np.uint8)
 
-        self.templates = list(
-            map(lambda template: pre_process(template), self.templates)
-        )
+        self.templates = list(map(lambda template: pre_process(template), self.templates))
 
     def __select_roi(self, target):
         n_frames = self.total_frames
@@ -76,20 +70,14 @@ class TemplateMatch:
     def template_match(self, frame, threshold=0.7, test=False):
         def pre_process(frame):
             frame = cv2.bitwise_not(frame)
-            newsize = (
-                self.TARGET_TIMESTAMP_SIZE
-                if self.element_type == "timestamp"
-                else self.TARGET_DATALOGGER_SIZE
-            )
-            frame = cv2.resize(frame, dsize=newsize,
-                               interpolation=cv2.INTER_CUBIC)
+            newsize = self.TARGET_TIMESTAMP_SIZE if self.element_type == "timestamp" else self.TARGET_DATALOGGER_SIZE
+            frame = cv2.resize(frame, dsize=newsize, interpolation=cv2.INTER_CUBIC)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             return frame.astype(np.uint8)
 
         def show(frame, tomatch, templates):
             """Shows templates, frame and frame ROI from which to extract the digits."""
-            _, ax = plt.subplots(nrows=1, ncols=len(
-                templates), figsize=(20, 30))
+            _, ax = plt.subplots(nrows=1, ncols=len(templates), figsize=(20, 30))
             for count, col in enumerate(ax):  # type: ignore
                 col.imshow(self.templates[count], cmap="gray")
                 col.set_title(f"template {count}")
@@ -108,12 +96,8 @@ class TemplateMatch:
 
         if len(self.templates[0].shape) > 2:
             self.__pre_process_templates()
-        top_x, top_y, bottom_x, bottom_y = (
-            self.timestamp_roi
-            if self.element_type == "timestamp"
-            else self.datalogger_roi
-        )
-        tomatch = frame[top_y: top_y + bottom_y, top_x: top_x + bottom_x]
+        top_x, top_y, bottom_x, bottom_y = self.timestamp_roi if self.element_type == "timestamp" else self.datalogger_roi
+        tomatch = frame[top_y : top_y + bottom_y, top_x : top_x + bottom_x]
         tomatch = pre_process(tomatch)
 
         if test:
@@ -127,9 +111,7 @@ class TemplateMatch:
         for template_number, _ in enumerate(self.templates):
             curr_template = self.templates[template_number]
             (tH, tW) = curr_template.shape[:2]
-            result = cv2.matchTemplate(
-                tomatch_copy, curr_template, cv2.TM_CCOEFF_NORMED
-            )
+            result = cv2.matchTemplate(tomatch_copy, curr_template, cv2.TM_CCOEFF_NORMED)
             (yCoords, xCoords) = np.where(result >= threshold)
 
             boxes = []
@@ -149,10 +131,7 @@ class TemplateMatch:
 
             if test:
                 for (startX, startY, endX, endY) in nms_boxes:
-                    cv2.rectangle(
-                        tomatch_copy, (startX, startY), (endX,
-                                                         endY), (0, 0, 0), 3
-                    )
+                    cv2.rectangle(tomatch_copy, (startX, startY), (endX, endY), (0, 0, 0), 3)
             if template_number == len(self.templates) - 1:
                 plt.figure()
                 plt.imshow(tomatch_copy, cmap="gray")
@@ -174,9 +153,7 @@ class TemplateMatch:
 
         except Exception:
             traceback.print_exc()
-            print(
-                f"[ERROR] starts is empty! Zero bounding boxes drawn for this {self.element_type}."
-            )
+            print(f"[ERROR] starts is empty! Zero bounding boxes drawn for this {self.element_type}.")
 
     def extract_timestamps(self):
         if not self.timestamp_roi:
